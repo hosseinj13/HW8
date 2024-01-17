3fe929f6f088aad6027ea19080dfae87d3b88467
hossein javadi <hosseinj13@yahoo.com>
2024-01-18 01:18:27 +0330

    create static fields in the ApplicationContext.java

--- src/main/java/utility/ApplicationContext.java
+++ src/main/java/utility/ApplicationContext.java
@@ -1,4 +1,21 @@
 package utility;
 
+import connection.JdbcConnection;
+import repository.UserRepository;
+import service.UserService;
+import java.sql.Connection;
+
+
 public class ApplicationContext {
+
+    private static final Connection CONNECTION;
+    private static final UserRepository USER_REPOSITORY;
+    private static final UserService USER_SERVICE;
+
+
+    static{
+        CONNECTION = JdbcConnection.getConnection();
+        USER_REPOSITORY = new UserRepository(CONNECTION);
+        USER_SERVICE = new UserService(USER_REPOSITORY);
+    }
 }
