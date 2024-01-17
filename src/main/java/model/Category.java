57243128f5fe8c9828fa457f8a15810bf71fbbe2
hossein javadi <hosseinj13@yahoo.com>
2024-01-17 23:09:07 +0330

    create Constructors in Category.java

--- src/main/java/model/Category.java
+++ src/main/java/model/Category.java
@@ -4,4 +4,13 @@ public class Category {
     private int categoryId;
     private String categoryName;
     private String description;
+
+    public Category() {
+    }
+
+    public Category(int categoryId, String categoryName, String description) {
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+        this.description = description;
+    }
 }
