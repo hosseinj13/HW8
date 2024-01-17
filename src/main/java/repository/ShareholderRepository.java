140291e43d76121b7d7127bc91796c9c94ea803a
hossein javadi <hosseinj13@yahoo.com>
2024-01-18 02:15:00 +0330

    create final connection field and create constructor in the ShareholderRepository.java

--- src/main/java/repository/ShareholderRepository.java
+++ src/main/java/repository/ShareholderRepository.java
@@ -1,4 +1,11 @@
 package repository;
 
+import java.sql.Connection;
+
 public class ShareholderRepository {
+
+    private final Connection connection;
+    public ShareholderRepository(Connection connection){
+        this.connection = connection;
+    }
 }
