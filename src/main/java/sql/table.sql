3f98824a98495786299254f42c0e2eb0edabec4a
hossein javadi <hosseinj13@yahoo.com>
2024-01-17 22:08:32 +0330

    create table.sql file

---
+++ src/main/java/sql/table.sql
@@ -0,0 +1,51 @@
+CREATE TABLE IF NOT EXISTS users
+(
+    user_id  SERIAL PRIMARY KEY,
+    name     VARCHAR(255)        NOT NULL,
+    username VARCHAR(50) UNIQUE  NOT NULL,
+    email    VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255)        NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS category
+(
+    category_id SERIAL PRIMARY KEY,
+    description VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS product
+(
+    product_id  SERIAL PRIMARY KEY,
+    name        VARCHAR(255) NOT NULL,
+    create_date DATE,
+    category_id INT,
+    brand_id    INT,
+    FOREIGN KEY (category_id) REFERENCES category (category_id),
+    FOREIGN KEY (brand_id) REFERENCES brand (brand_id)
+);
+
+CREATE TABLE IF NOT EXISTS brand
+(
+    brand_id    SERIAL PRIMARY KEY,
+    name        VARCHAR(255) NOT NULL,
+    website     VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS shareholder_brand
+(
+    shareholder_id INT,
+    brand_id       INT,
+    FOREIGN KEY (shareholder_id) REFERENCES shareholder (shareholder_id),
+    FOREIGN KEY (brand_id) REFERENCES brand (brand_id)
+);
+
+CREATE TABLE IF NOT EXISTS shareholder
+(
+    shareholder_id SERIAL PRIMARY KEY,
+    name           VARCHAR(255) NOT NULL,
+    phone_number   VARCHAR(15) UNIQUE,
+    national_code  VARCHAR(10) UNIQUE
+);
+
+
