b612956e9359e422d1a861fbfb88273050cee4ac
hossein javadi <hosseinj13@yahoo.com>
2024-01-17 23:10:22 +0330

    create Constructors in Product.java

--- src/main/java/model/Product.java
+++ src/main/java/model/Product.java
@@ -10,5 +10,14 @@ public class Product {
     private int categoryId;
     private int brandId;
 
+    public Product() {
+    }
 
+    public Product(int productId, String productName, Date createDate, int categoryId, int brandId) {
+        this.productId = productId;
+        this.productName = productName;
+        this.createDate = createDate;
+        this.categoryId = categoryId;
+        this.brandId = brandId;
+    }
 }
