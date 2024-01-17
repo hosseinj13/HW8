fdcb7fa3c9df0d768032830e1acb3b6dbadaff39
hossein javadi <hosseinj13@yahoo.com>
2024-01-17 23:21:20 +0330

    create and add getter and setter in Shareholder.java

--- src/main/java/model/Shareholder.java
+++ src/main/java/model/Shareholder.java
@@ -15,4 +15,36 @@ public class Shareholder {
         this.phoneNumber = phoneNumber;
         this.nationalCode = nationalCode;
     }
+
+    public int getShareholderId() {
+        return shareholderId;
+    }
+
+    public void setShareholderId(int shareholderId) {
+        this.shareholderId = shareholderId;
+    }
+
+    public String getShareholderName() {
+        return shareholderName;
+    }
+
+    public void setShareholderName(String shareholderName) {
+        this.shareholderName = shareholderName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getNationalCode() {
+        return nationalCode;
+    }
+
+    public void setNationalCode(String nationalCode) {
+        this.nationalCode = nationalCode;
+    }
 }
