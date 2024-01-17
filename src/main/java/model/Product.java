2348a3269a5a85464c3e0defa5fe9cc656ad6e0e
hossein javadi <hosseinj13@yahoo.com>
2024-01-17 23:00:37 +0330

    create and add fields in Product.java

--- src/main/java/model/Product.java
+++ src/main/java/model/Product.java
@@ -5,8 +5,8 @@ import java.util.Date;
 public class Product {
 
     private int productId;
-    private String name;
-    private Date create_date;
+    private String productName;
+    private Date createDate;
     private int categoryId;
     private int brandId;
 
