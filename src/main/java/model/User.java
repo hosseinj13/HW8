c7a01d3141033ebdcbf37f37c30687acc43de3b2
hossein javadi <hosseinj13@yahoo.com>
2024-01-17 22:47:00 +0330

    create and add fields in User.java

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -2,4 +2,12 @@ package model;
 
 public class User {
 
+    private int userId;
+    private String name;
+    private String username;
+    private String email;
+    private String password;
+
+
+
 }
