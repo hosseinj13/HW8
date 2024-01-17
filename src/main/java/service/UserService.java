33c661315a1540b3b2e8c098b969822daa305052
hossein javadi <hosseinj13@yahoo.com>
2024-01-18 01:28:42 +0330

    create final field type of UserRepository and create constructor in the UserService.java

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,4 +1,11 @@
 package service;
 
+import repository.UserRepository;
+
 public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 }
