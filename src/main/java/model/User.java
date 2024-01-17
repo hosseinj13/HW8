2b1b01d6660f706acd9f0299aba8ceb6de3886b5
hossein javadi <hosseinj13@yahoo.com>
2024-01-17 23:16:45 +0330

    create and add getter and setter in User.java

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -19,4 +19,45 @@ public class User {
         this.email = email;
         this.password = password;
     }
+
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
