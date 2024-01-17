491e12c4f036ee2998298038588252c99f123c19
hossein javadi <hosseinj13@yahoo.com>
2024-01-18 02:08:28 +0330

    create final field type of CategoryRepository and create constructor in the CategoryService.java

--- src/main/java/service/CategoryService.java
+++ src/main/java/service/CategoryService.java
@@ -1,4 +1,13 @@
 package service;
 
+import repository.CategoryRepository;
+import repository.UserRepository;
+
 public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public CategoryService(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
 }
