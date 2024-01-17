238e77050f26a13b4cc0177e7b4677bbc334784f
hossein javadi <hosseinj13@yahoo.com>
2024-01-18 02:19:22 +0330

    create final field type of ShareholderRepository and create constructor in the ShareholderService.java

--- src/main/java/service/ShareholderService.java
+++ src/main/java/service/ShareholderService.java
@@ -1,4 +1,13 @@
 package service;
 
+import repository.ShareholderRepository;
+import repository.UserRepository;
+
 public class ShareholderService {
+
+    private final ShareholderRepository shareholderRepository;
+
+    public ShareholderService(ShareholderRepository shareholderRepository) {
+        this.shareholderRepository = shareholderRepository;
+    }
 }
