c3645e6797eb2a821fda3a86c6e0e11af00ef8dd
hossein javadi <hosseinj13@yahoo.com>
2024-01-18 01:44:18 +0330

    create final field type of BrandRepository and create constructor in the BrandService.java

--- src/main/java/service/BrandService.java
+++ src/main/java/service/BrandService.java
@@ -1,4 +1,12 @@
 package service;
 
+import repository.UserRepository;
+
 public class BrandService {
+
+    private final UserRepository userRepository;
+
+    public BrandService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 }
