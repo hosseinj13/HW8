9bcf52086fc0b04ebb8cf57c54b4aa6393d2c168
hossein javadi <hosseinj13@yahoo.com>
2024-01-18 01:40:40 +0330

    create static fields for Brand  and create getBrandService that is static method in the ApplicationContext.java

--- src/main/java/utility/ApplicationContext.java
+++ src/main/java/utility/ApplicationContext.java
@@ -1,7 +1,9 @@
 package utility;
 
 import connection.JdbcConnection;
+import repository.BrandRepository;
 import repository.UserRepository;
+import service.BrandService;
 import service.UserService;
 import java.sql.Connection;
 
@@ -10,17 +12,31 @@ public class ApplicationContext {
 
     private static final Connection CONNECTION;
     private static final UserRepository USER_REPOSITORY;
+    private static final BrandRepository BRAND_REPOSITORY;
+
+
     private static final UserService USER_SERVICE;
+    private static final BrandService BRAND_SERVICE;
+
 
 
     static{
         CONNECTION = JdbcConnection.getConnection();
         USER_REPOSITORY = new UserRepository(CONNECTION);
         USER_SERVICE = new UserService(USER_REPOSITORY);
+
+        BRAND_REPOSITORY = new BrandRepository(CONNECTION);
+        BRAND_SERVICE = new BrandService(BRAND_REPOSITORY);
+
     }
 
     public static UserService getUserService(){
         return USER_SERVICE;
     }
+
+    public static UserService getBrandService(){
+        return BRAND_SERVICE;
+    }
+
 
 }
