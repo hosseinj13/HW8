c2c03119efc9aa21c20da2c1048f2c0a8d233e41
hossein javadi <hosseinj13@yahoo.com>
2024-01-17 23:04:55 +0330

    create and add fields in Shareholder.java

--- src/main/java/model/Shareholder.java
+++ src/main/java/model/Shareholder.java
@@ -1,4 +1,9 @@
 package model;
 
 public class Shareholder {
+    private int shareholderId;
+    private String shareholderName;
+    private String phoneNumber;
+    private String nationalCode;
+
 }
