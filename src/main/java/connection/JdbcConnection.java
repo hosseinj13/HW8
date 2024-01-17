4d28c1cd3be7bf2565c830e3b1478fc34064326e
hossein javadi <hosseinj13@yahoo.com>
2024-01-18 00:49:14 +0330

    create static final field type of Connection in this JdbcConnection.java

--- src/main/java/connection/JdbcConnection.java
+++ src/main/java/connection/JdbcConnection.java
@@ -1,4 +1,25 @@
 package connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JdbcConnection {
+
+    private static final Connection connection;
+
+    static {
+        try {
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/maktab", "postgres", "9010231394");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public JdbcConnection() {
+    }
+
+    static public Connection getConnection() {
+        return connection;
+    }
 }
