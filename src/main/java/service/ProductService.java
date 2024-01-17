d2e36a9f18edaef670560c0a8b1c477fb2c0b547
hossein javadi <hosseinj13@yahoo.com>
2024-01-18 02:00:37 +0330

    create final field type of ProductRepository and create constructor in the ProductService.java

--- src/main/java/service/ProductService.java
+++ src/main/java/service/ProductService.java
@@ -1,4 +1,13 @@
 package service;
 
+import repository.ProductRepository;
+import repository.UserRepository;
+
 public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
 }
