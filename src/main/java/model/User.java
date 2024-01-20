58fb2b3198988a671a757d14ff991495d2b8cb25
hossein javadi <hosseinj13@yahoo.com>
2024-01-20 12:20:31 +0330

    Fixed User.java class in the model package with lombok library(Lombok Annotation).

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -1,4 +1,17 @@
 package model;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
 public class User {
+
+    private int userId;
+    private String name;
+    private String userName;
+    private String password;
 }
