a985e3c127e07e420a6e5e8e13eb745fc0736ae0
hossein javadi <hosseinj13@yahoo.com>
2024-01-20 12:35:42 +0330

    create ShareholderBrandRepository.java class in the repository package and add the constructor for connection config

---
+++ src/main/java/repository/ShareholderBrandRepository.java
@@ -0,0 +1,11 @@
+package repository;
+
+import java.sql.Connection;
+
+public class ShareholderBrandRepository {
+
+    private final Connection connection;
+    public ShareholderBrandRepository(Connection connection){
+        this.connection = connection;
+    }
+}
