474eb30d89c6d93025e9e4d0ce4748d28a3ec7bf
hossein javadi <hosseinj13@yahoo.com>
2024-01-20 12:20:13 +0330

    Fixed Shareholder.java class in the model package with lombok library(Lombok Annotation).

--- src/main/java/model/Shareholder.java
+++ src/main/java/model/Shareholder.java
@@ -1,50 +1,17 @@
 package model;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
 public class Shareholder {
     private int shareholderId;
     private String shareholderName;
     private String phoneNumber;
     private String nationalCode;
 
-    public Shareholder() {
-    }
-
-    public Shareholder(int shareholderId, String shareholderName, String phoneNumber, String nationalCode) {
-        this.shareholderId = shareholderId;
-        this.shareholderName = shareholderName;
-        this.phoneNumber = phoneNumber;
-        this.nationalCode = nationalCode;
-    }
-
-    public int getShareholderId() {
-        return shareholderId;
-    }
-
-    public void setShareholderId(int shareholderId) {
-        this.shareholderId = shareholderId;
-    }
-
-    public String getShareholderName() {
-        return shareholderName;
-    }
-
-    public void setShareholderName(String shareholderName) {
-        this.shareholderName = shareholderName;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getNationalCode() {
-        return nationalCode;
-    }
-
-    public void setNationalCode(String nationalCode) {
-        this.nationalCode = nationalCode;
-    }
 }
