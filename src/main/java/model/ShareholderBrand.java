568f5cebdb7727791aeca128166a4e953977accb
hossein javadi <hosseinj13@yahoo.com>
2024-01-20 12:14:20 +0330

    create ShareholderBrand.java class and add fields, constructors, getter, setter with lombok library(lombok Annotation)

---
+++ src/main/java/model/ShareholderBrand.java
@@ -0,0 +1,17 @@
+package model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+public class ShareholderBrand {
+
+    private int shareholderId;
+    private int brandId;
+
+
+}
