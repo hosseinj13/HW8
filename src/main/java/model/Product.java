8dbece754e4174b3aa1470f633ccc989a85382b1
hossein javadi <hosseinj13@yahoo.com>
2024-01-20 12:19:44 +0330

    Fixed Product.java class in the model package with lombok library(Lombok Annotation).

--- src/main/java/model/Product.java
+++ src/main/java/model/Product.java
@@ -1,6 +1,14 @@
 package model;
 
+import lombok.*;
+
 import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 
 public class Product {
 
@@ -10,54 +18,5 @@ public class Product {
     private int categoryId;
     private int brandId;
 
-    public Product() {
-    }
 
-    public Product(int productId, String productName, Date createDate, int categoryId, int brandId) {
-        this.productId = productId;
-        this.productName = productName;
-        this.createDate = createDate;
-        this.categoryId = categoryId;
-        this.brandId = brandId;
-    }
-
-    public int getProductId() {
-        return productId;
-    }
-
-    public void setProductId(int productId) {
-        this.productId = productId;
-    }
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public void setProductName(String productName) {
-        this.productName = productName;
-    }
-
-    public Date getCreateDate() {
-        return createDate;
-    }
-
-    public void setCreateDate(Date createDate) {
-        this.createDate = createDate;
-    }
-
-    public int getCategoryId() {
-        return categoryId;
-    }
-
-    public void setCategoryId(int categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public int getBrandId() {
-        return brandId;
-    }
-
-    public void setBrandId(int brandId) {
-        this.brandId = brandId;
-    }
 }
