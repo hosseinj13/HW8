cc67b59fd5c4d061347deb225f26d9d09cda1213
hossein javadi <hosseinj13@yahoo.com>
2024-01-22 13:36:45 +0330

    Fixed and Update Category.java in the model package.

--- src/main/java/model/Category.java
+++ src/main/java/model/Category.java
@@ -13,4 +13,15 @@ public class Category {
     private int categoryId;
     private String categoryName;
     private String description;
+
+    public Category(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public Category(String categoryName, String description) {
+        this.categoryName = categoryName;
+        this.description = description;
+    }
+
+
 }
