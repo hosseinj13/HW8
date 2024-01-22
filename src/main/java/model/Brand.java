3ada985faba68f32022cb852ddb98597cdb465fd
hossein javadi <hosseinj13@yahoo.com>
2024-01-22 13:36:15 +0330

    Fixed and Update Brand.java in the model package.

--- src/main/java/model/Brand.java
+++ src/main/java/model/Brand.java
@@ -11,10 +11,24 @@ import lombok.*;
 
 public class Brand {
     private int brandId;
+
     private String brandName;
 
     private String website;
 
     private String description;
 
+    public Brand(int brandId) {
+        this.brandId = brandId;
+    }
+
+    public Brand(String brandName) {
+        this.brandName = brandName;
+    }
+
+    public Brand(String brandName, String website, String description) {
+        this.brandName = brandName;
+        this.website = website;
+        this.description = description;
+    }
 }
