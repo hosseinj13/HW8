0419c53a68f7eb0082b797ad64941bb7a8c4c03e
hossein javadi <hosseinj13@yahoo.com>
2024-01-22 13:35:24 +0330

    Fixed and Update User.java in the model package.

--- src/main/java/model/User.java
+++ src/main/java/model/User.java
@@ -13,5 +13,18 @@ public class User {
     private int userId;
     private String name;
     private String userName;
+    private String email;
     private String password;
+
+    public User(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public User(String name, String userName, String email, String password) {
+        this.name = name;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
 }
