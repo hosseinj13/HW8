ed1437a036ab31bf1d73263c5c53187dd971b122
hossein javadi <hosseinj13@yahoo.com>
2024-01-22 23:12:40 +0330

    Fixed Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,16 +7,6 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) throws SQLException {
         Menu menu = new Menu();
-        menu.userMenu();
-        System.out.println("**********");
-        menu.categoryMenu();
-        System.out.println("**********");
-        menu.brandMenu();
-        System.out.println("**********");
-        menu.productMenu();
-        System.out.println("**********");
-        menu.shareholderMenu();
-        System.out.println("**********");
-        menu.shareholderBrandMenu();
+        menu.publicMenu();
     }
 }
