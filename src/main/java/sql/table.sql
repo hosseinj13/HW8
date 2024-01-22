1172167ff4eb063298a565ac1174ca1f052bba91
hossein javadi <hosseinj13@yahoo.com>
2024-01-22 13:33:55 +0330

    Fixed and update table.sql file

--- src/main/java/sql/table.sql
+++ src/main/java/sql/table.sql
@@ -9,17 +9,18 @@ CREATE TABLE IF NOT EXISTS users
 
 CREATE TABLE IF NOT EXISTS category
 (
-    category_id SERIAL PRIMARY KEY,
-    description VARCHAR(255)
+    category_id   SERIAL PRIMARY KEY,
+    category_name VARCHAR(50) UNIQUE NOT NULL,
+    description   VARCHAR(255)
 );
 
 CREATE TABLE IF NOT EXISTS product
 (
-    product_id  SERIAL PRIMARY KEY,
-    name        VARCHAR(255) NOT NULL,
-    create_date DATE,
-    category_id INT,
-    brand_id    INT,
+    product_id   SERIAL PRIMARY KEY,
+    product_name VARCHAR(255) NOT NULL,
+    create_date  DATE,
+    category_id  INT,
+    brand_id     INT,
     FOREIGN KEY (category_id) REFERENCES category (category_id),
     FOREIGN KEY (brand_id) REFERENCES brand (brand_id)
 );
@@ -27,9 +28,9 @@ CREATE TABLE IF NOT EXISTS product
 CREATE TABLE IF NOT EXISTS brand
 (
     brand_id    SERIAL PRIMARY KEY,
-    name        VARCHAR(255) NOT NULL,
-    website     VARCHAR(255) NOT NULL,
-    description VARCHAR(255) NOT NULL
+    brand_name  VARCHAR(255) UNIQUE NOT NULL,
+    website     VARCHAR(255)        NOT NULL,
+    description VARCHAR(255)        NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS shareholder_brand
@@ -42,8 +43,8 @@ CREATE TABLE IF NOT EXISTS shareholder_brand
 
 CREATE TABLE IF NOT EXISTS shareholder
 (
-    shareholder_id SERIAL PRIMARY KEY,
-    name           VARCHAR(255) NOT NULL,
-    phone_number   VARCHAR(15) UNIQUE,
-    national_code  VARCHAR(10) UNIQUE
+    shareholder_id   SERIAL PRIMARY KEY,
+    shareholder_name VARCHAR(255) NOT NULL,
+    phone_number     VARCHAR(15) UNIQUE,
+    national_code    VARCHAR(10) UNIQUE
 );
