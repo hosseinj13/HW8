f86abeb4506a279dd768fb388f9d35b7c975c8cd
hossein javadi <hosseinj13@yahoo.com>
2024-01-22 13:47:12 +0330

    create and add (SIGN UP & SIGN IN METHOD) to the UserRepository.java in the repository package.

--- src/main/java/repository/UserRepository.java
+++ src/main/java/repository/UserRepository.java
@@ -1,13 +1,68 @@
 package repository;
 
+import model.User;
+
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class UserRepository {
 
     private final Connection connection;
-    public UserRepository(Connection connection){
+
+    public UserRepository(Connection connection) {
         this.connection = connection;
     }
+
+    public void save(User user) throws SQLException {
+
+        String addUser = "INSERT INTO maktab.hw8.users (name, username, email, password) VALUES (?, ?, ?, ?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(addUser);
+
+        preparedStatement.setString(1, user.getName());
+        preparedStatement.setString(2, user.getUserName());
+        preparedStatement.setString(3, user.getEmail());
+        preparedStatement.setString(4, user.getPassword());
+        int resultSet = preparedStatement.executeUpdate();
+    }
+
+
+    public User findByUsername(String username) throws SQLException {
+
+        String findUser = "SELECT * FROM maktab.hw8.users WHERE username = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(findUser);
+        preparedStatement.setString(1, username);
+        ResultSet resultSet = preparedStatement.executeQuery();
+        if (resultSet.next()) {
+            int id = resultSet.getInt("user_id");
+            String name = resultSet.getString("name");
+            String fetchUsername = resultSet.getString("username");
+            String email = resultSet.getString("email");
+            String password = resultSet.getString("password");
+            User user = new User(name, fetchUsername, email, password);
+            return user;
+        } else
+            return null;
+    }
+
+
+   /* public boolean userUniqe(String username) throws SQLException {
+
+        String sql = "select username from users where " +
+                "exists(select username from users where username = ?)";
+
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setString(1, username);
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        if (resultSet.next()) {
+            return true;
+        } else {
+            return false;
+        }
+    }*/
 
 
 }
